e57cb35238ad6aab87fe63a0820a19cd817899ef
EmilBotezatuMIXCONIX <103512920+EmilBotezatuMIXCONIX@users.noreply.github.com>
2022-05-22 22:17:15 +0300

    Imaginile pulli

--- Hotel/Hotel/ViewModel/ClientViewModel.cs
+++ Hotel/Hotel/ViewModel/ClientViewModel.cs
@@ -20,7 +20,7 @@ namespace Hotel.ViewModel
             room1.price = 750;
             room1.type = "single";
             Picture picture1 = new Picture();
-            picture1.url = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fstudioinsign.ro%2Fwp-content%2Fuploads%2F2019%2F03%2Fproiect-camera-hotel.jpg&imgrefurl=https%3A%2F%2Fstudioinsign.ro%2Fproiect%2Fcamera-de-hotel-in-stil-modern-contemporan-cu-accente-luxury%2F&tbnid=4wmX57P5bJ6qiM&vet=12ahUKEwj4pefPl_P3AhVDKewKHY83CMkQMygAegUIARDHAQ..i&docid=NENfVe-_CKCNCM&w=1500&h=831&q=camera%20hotel&client=firefox-b-d&ved=2ahUKEwj4pefPl_P3AhVDKewKHY83CMkQMygAegUIARDHAQ";
+            picture1.url = "https://www.itstactical.com/wp-content/uploads/2016/10/Hotel-Security-featured.jpg";
             room1.Pictures.Add(picture1);
 
 
@@ -29,12 +29,14 @@ namespace Hotel.ViewModel
             room2.number = 2;
             room2.price = 550;
             room2.type = "quad";
-            picture2.url = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcomforty.ro%2Fwp-content%2Fuploads%2F2018%2F10%2Fmobilier-camere-hotel-1024x607.jpg&imgrefurl=https%3A%2F%2Fcomforty.ro%2Fmobilier-camere-hotel%2F&tbnid=S-TrJWWBqvpuuM&vet=12ahUKEwj4pefPl_P3AhVDKewKHY83CMkQMygBegUIARDJAQ..i&docid=nw9LqdmU0y3fHM&w=1024&h=607&q=camera%20hotel&client=firefox-b-d&ved=2ahUKEwj4pefPl_P3AhVDKewKHY83CMkQMygBegUIARDJAQ";
+            picture2.url = "https://www.itstactical.com/wp-content/uploads/2016/10/Hotel-Security-featured.jpg";
             room2.Pictures.Add(picture2);
             //Rooms = new ObservableCollection<Room>();
             Rooms.Add(room1);
             Rooms.Add(room2);
         }
-
+      
     }
+   
+   
 }
