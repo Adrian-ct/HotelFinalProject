5850197dad0ca70a9a250f53ef582e57f6544f47
Leo <61775738+Leobej@users.noreply.github.com>
2022-05-22 18:47:31 +0300

    Added Register

--- Hotel/Hotel/ViewModel/RegisterViewModel.cs
+++ Hotel/Hotel/ViewModel/RegisterViewModel.cs
@@ -1,38 +1,97 @@
-﻿using Hotel.Model;
+﻿using Hotel.Help;
+using Hotel.Model;
+using Hotel.Models;
+using Hotel.Models.BusinessLogicLayer;
+using Hotel.View;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
-
+using Hotel.Utilities;
 namespace Hotel.ViewModel
 {
-    internal class RegisterViewModel
+    internal class RegisterViewModel : ObservableObject
     {
         public ICommand BackCommand { get; }
-
-        public List<UserM> Users { get; set; }
-
+        public ICommand RegisterCommand { get; }
+        UserBLL userBLL = new UserBLL();
+        User user = new User();
         public RegisterViewModel()
         {
-
-            Users = new List<UserM> {
-                new UserM
-            {
-            Username="leobej",
-            First_name="Leo",
-            Last_name="Bejgu",
-            }
-            };
-
+            BackCommand = new RelayCommands(Back);
+            RegisterCommand = new RelayCommands(Register);
         }
+
         public void Back()
         {
             MainWindow mainWindow = new MainWindow();
-            App.Current.MainWindow.Close();
             App.Current.MainWindow = mainWindow;
             mainWindow.Show();
+        }
+
+        public void Register()
+        {
+            //user.username = Username;
+            //if (1==2)
+            //{
+            //    MessageBox.Show("Username exists!");
+            //}
+            //else
+            {
+                user.username = Username;
+                user.first_name = FirstName;
+                user.last_name = LastName;
+                user.role = Role;
+
+                switch (Role)
+                {
+                    case "0":
+                        user.role = "admin";
+                        break;
+                    case "1":
+                        user.role = "employee";
+                        break;
+                    case "2":
+                        user.role = "client";
+                        break;
+                }
+                userBLL.RegisterMethod(user);
+            }
+        }
+
+
+        private string username;
+        public string Username
+        {
+            get { return username; }
+            set { OnPropertyChanged(ref username, value); }
+        }
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set { OnPropertyChanged(ref password, value); }
+        }
+        private string firstName;
+        public string FirstName
+        {
+            get { return firstName; }
+            set { OnPropertyChanged(ref firstName, value); }
+        }
+        private string lastName;
+        public string LastName
+        {
+            get { return lastName; }
+            set { OnPropertyChanged(ref lastName, value); }
+        }
+        private string role;
+        public string Role
+        {
+            get { return role; }
+            set { OnPropertyChanged(ref role, value); }
         }
     }
 }
