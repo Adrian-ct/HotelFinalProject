f3411f39696a0e1a52edf51043e30b5a5cf179e3
Leo <61775738+Leobej@users.noreply.github.com>
2022-05-23 13:24:08 +0300

    Added DetailWindow and Class

---
+++ Hotel/Hotel/ViewModel/DetailsViewModel.cs
@@ -0,0 +1,71 @@
+﻿using Hotel.Models.BusinessLogicLayer;
+using Hotel.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Hotel.ViewModel
+{
+    internal class DetailsViewModel : ObservableObject
+    {
+        static RoomBLL room = new RoomBLL();
+        static public List<Hotel.Models.Room> roomsList = room.GetAllRooms();
+        public DetailsViewModel()
+        {
+            string roomData = roomsList[index].ToString();
+            imageSource = roomData.Substring(0, 1);
+        }
+
+        private string imageSource;
+        public string ImageSource
+        {
+            get
+            {
+                return imageSource;
+            }
+            set
+            {
+                imageSource = value;
+                //if (editMode)
+                //{
+                //    CanExecuteCommandSignIn = Validators.CanExecutePlay(NameTextBox);
+                //}
+                //CanExecuteCommandNext = Validators.CanExecuteNext(imageSource, images);
+                //CanExecuteCommandPrev = Validators.CanExecutePrev(imageSource, images);
+                NotifyPropertyChanged("ImageSource");
+            }
+        }
+        private string features;
+        public string Features
+        {
+            get
+            {
+                return features;
+            }
+            set
+            {
+                features = value;
+                //if (editMode)
+                //{
+                //    CanExecuteCommandSignIn = Validators.CanExecutePlay(NameTextBox);
+                //}
+                //CanExecuteCommandNext = Validators.CanExecuteNext(imageSource, images);
+                //CanExecuteCommandPrev = Validators.CanExecutePrev(imageSource, images);
+                NotifyPropertyChanged("ImageSource");
+            }
+        }
+        public int index;
+        public int Index
+        {
+            get
+            {
+                return index;
+            }
+            set
+            {
+                index = value;
+            }
+        }
+    }
+}
